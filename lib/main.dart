fc053de0ef7c167bfa5f64b4b4d92e4fa036eae3
Tolulope <lanre.toluwa@gmail.com>
2021-03-27 17:16:49 +0100

    convert MyApp to StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,29 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Forex Rates",
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Foreign Exchange Rates"),
+          ),
+          body: Center(
+            child: Text("Find conversion rates for any currency pair"),
+          )),
+    );
+  }
+}
+
+/*
+class MyApp extends StatefulWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,3 +39,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+*/
