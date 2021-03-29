7d59db7d0e57e21177219e251cc3195268bc595b
Tolulope <lanre.toluwa@gmail.com>
2021-03-29 12:01:48 +0100

    created required widgets

--- lib/main.dart
+++ lib/main.dart
@@ -8,39 +8,118 @@ class MyApp extends StatefulWidget {
 }
 
 class MyAppState extends State<MyApp> {
-  double _numberForm;
+  double _amountToConvert;
+  final List<String> _rates = [
+    "Dollar (US)",
+    "Pound",
+    "Euro",
+    "Japanese Yen",
+    "Bitcoin",
+    "Naira",
+  ];
+
+  String _fromCurrency;
+  String _toCurrency;
+
+  // TextStyles
+  final TextStyle fieldStyle = TextStyle(
+    fontSize: 20,
+    color: Colors.blue[900],
+  );
+
+  final TextStyle labelStyle = TextStyle(
+    fontSize: 24,
+    color: Colors.grey[700],
+  );
 
   @override
   void initState() {
-    _numberForm = 0;
+    _amountToConvert = 0;
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Forex Rates",
-      home: Scaffold(
+        title: "Forex Rates",
+        home: Scaffold(
           appBar: AppBar(
             title: Text("Foreign Exchange Rates"),
           ),
-          body: Center(
+          body: Container(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: SingleChildScrollView(
               child: Column(
-            children: [
-              TextField(
-                onChanged: (text) {
-                  var userInput = double.tryParse(text);
-                  if (userInput != null) {
-                    setState(() {
-                      _numberForm = userInput;
-                    });
-                  }
-                },
+                children: [
+                  Text(
+                    "Result",
+                    style: labelStyle,
+                  ),
+                  TextField(
+                    style: fieldStyle,
+                    decoration: InputDecoration(
+                        hintText: "Please, Enter the amount to be converted"),
+                    onChanged: (text) {
+                      var userInput = double.tryParse(text);
+                      if (userInput != null) {
+                        setState(() {
+                          _amountToConvert = userInput;
+                        });
+                      }
+                    },
+                  ),
+                  Text(
+                    "From",
+                    style: labelStyle,
+                  ),
+                  DropdownButton<String>(
+                    isExpanded: true,
+                    items: _rates.map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        _fromCurrency = value;
+                      });
+                    },
+                    value: _fromCurrency,
+                  ),
+                  Text(
+                    "To",
+                    style: labelStyle,
+                  ),
+                  DropdownButton<String>(
+                    items: _rates.map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        _toCurrency = value;
+                      });
+                    },
+                    value: _toCurrency,
+                  ),
+                  RaisedButton(
+                    child: Text(
+                      "CONVERT",
+                      style: fieldStyle,
+                    ),
+                    onPressed: () => true,
+                  ),
+                  Text((_amountToConvert == null)
+                      ? ''
+                      : _amountToConvert.toString()),
+                ],
               ),
-              Text((_numberForm == null) ? '' : _numberForm.toString())
-            ],
-          ))),
-    );
+            ),
+          ),
+        ));
   }
 }
 
